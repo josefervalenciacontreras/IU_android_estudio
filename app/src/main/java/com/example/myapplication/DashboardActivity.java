c48dcca47103aa052c6e31fd33203ad45eeffd64
josefervalenciacontreras <fernando.valencia@est.iudigital.edu.co>
2024-04-16 21:54:40 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/myapplication/MainActivity.java

--- app/src/main/java/com/example/myapplication/DashboardActivity.java
+++ app/src/main/java/com/example/myapplication/DashboardActivity.java
@@ -7,19 +7,11 @@ import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    TextView userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-        userName = findViewById(R.id.textUser);
-        Bundle bundle = getIntent().getExtras();
-        userName.setText(bundle.getString("user"));
-
-
-
 
     }
 }
